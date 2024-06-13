f19f0c8827b8677328617c388bfd84a0dbae507a
Nimastic <jerielchanzy@gmail.com>
2024-06-13 08:25:07 +0800

    commented

--- api/count.js
+++ api/count.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-const countFilePath = path.join(__dirname, 'count.json');
+// const countFilePath = path.join(__dirname, 'count.json');
 
-// Ensure the count.json file exists
-if (!fs.existsSync(countFilePath)) {
-    fs.writeFileSync(countFilePath, JSON.stringify({ count: 0 }));
-}
+// // Ensure the count.json file exists
+// if (!fs.existsSync(countFilePath)) {
+//     fs.writeFileSync(countFilePath, JSON.stringify({ count: 0 }));
+// }
 
-// Function to read the current count from file
-const getCountFromFile = () => {
-    const data = fs.readFileSync(countFilePath);
-    return JSON.parse(data).count;
-};
+// // Function to read the current count from file
+// const getCountFromFile = () => {
+//     const data = fs.readFileSync(countFilePath);
+//     return JSON.parse(data).count;
+// };
 
-module.exports = (req, res) => {
-    const count = getCountFromFile();
-    res.json({ count });
-};
+// module.exports = (req, res) => {
+//     const count = getCountFromFile();
+//     res.json({ count });
+// };
--- api/visit.js
+++ api/visit.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-const countFilePath = path.join(__dirname, 'count.json');
+// const countFilePath = path.join(__dirname, 'count.json');
 
-// Ensure the count.json file exists
-if (!fs.existsSync(countFilePath)) {
-    fs.writeFileSync(countFilePath, JSON.stringify({ count: 0 }));
-}
+// // Ensure the count.json file exists
+// if (!fs.existsSync(countFilePath)) {
+//     fs.writeFileSync(countFilePath, JSON.stringify({ count: 0 }));
+// }
 
-// Function to read the current count from file
-const getCountFromFile = () => {
-    const data = fs.readFileSync(countFilePath);
-    return JSON.parse(data).count;
-};
+// // Function to read the current count from file
+// const getCountFromFile = () => {
+//     const data = fs.readFileSync(countFilePath);
+//     return JSON.parse(data).count;
+// };
 
-// Function to save the current count to file
-const saveCountToFile = (count) => {
-    fs.writeFileSync(countFilePath, JSON.stringify({ count }));
-};
+// // Function to save the current count to file
+// const saveCountToFile = (count) => {
+//     fs.writeFileSync(countFilePath, JSON.stringify({ count }));
+// };
 
-module.exports = (req, res) => {
-    let count = getCountFromFile();
-    count += 1;
-    saveCountToFile(count);
-    res.json({ count });
-};
+// module.exports = (req, res) => {
+//     let count = getCountFromFile();
+//     count += 1;
+//     saveCountToFile(count);
+//     res.json({ count });
+// };
